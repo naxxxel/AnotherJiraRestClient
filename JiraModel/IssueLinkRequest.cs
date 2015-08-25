30a8bcc14cd699668c23bb05e61940365c390899
Alexander Bender <a.bender@mps-solutions.de>
2015-08-25 09:15:46 +0200

    Initial support for issue links, setting issue versions

---
+++ JiraModel/IssueLinkRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnotherJiraRestClient
+{
+    public class IssueLinkRequest
+    {
+        // JSON: type:name
+        public string TypeName { get; set; }
+        // JSON: inwardIssue:key
+        public string InwardIssueKey { get; set; }
+        // JSON: outwardIssue:key
+        public string OutwardIssueKey { get; set; }
+        public Comment Comment { get; set; }
+
+        public object GetAsBodyObject()
+        {
+            return new
+            {
+                type = new
+                {
+                    name = TypeName
+                },
+                inwardIssue = new
+                {
+                    key = InwardIssueKey
+                },
+                outwardIssue = new
+                {
+                    key = OutwardIssueKey
+                },
+                comment = Comment
+            };
+        }
+    }
+}
---
+++ JiraModel/IssueLinkTypes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnotherJiraRestClient.JiraModel
+{
+    public class IssueLinkTypes
+    {
+        public int startAt { get; set; }
+        public int maxResults { get; set; }
+        public int total { get; set; }
+        public List<IssueLinkRequest> linkTypes { get; set; }
+    }
+
+    public class IssueLink
+    {
+        public string type { get; set; }
+        public string name { get; set; }
+        public string inward { get; set; }
+        public string outward { get; set; }
+        public string self { get; set; }
+    }
+}
